c406f425b088ebe4432659aeb505012e8d9261a2
Matt Lopez <matthew.lopez4011@gmail.com>
2020-03-30 10:56:28 -0500

    add delete functionality

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import PostForm from '../components/PostForm';
+
 const Home = () => {
   // Array Destructuring
   const [posts, setPosts] = useState([]);
@@ -15,8 +17,27 @@ const Home = () => {
       .catch(err => console.error(err));
   }, []); // deps: empty array means it will fetch posts once.
 
+  function editPost(post){
+    console.log(post);
+  }
+
+  function deletePost(id){
+    axios.delete(`/post/${id}`)
+      .then(() => {
+        // Removes deleted post from array of posts.
+        const postsUpdated = posts.filter(p => p.id !== id);
+        setPosts(postsUpdated);
+      })
+      .catch(err => console.error(err));
+  }
+
   return (
     <div>
+        <div className="row">
+          <div className="col s6">
+            <PostForm/>
+          </div>
+        </div>
       <div className="row">
         {posts.map(post => (
           <div className="col s6" key={post.id}>
@@ -31,8 +52,9 @@ const Home = () => {
               <p>{post.body}</p>
             </div>
             <div className="card-action">
-              <a href="#">Edit</a>
-              <a href="#" className={"delete-btn"}>Delete</a>
+              {/*.bind: on the page load the edit function will be sent null(We don't want the function to run on load), once the edit btn is clicked the editPost function will be sent the post to edit*/}
+              <a href="#" onClick={editPost.bind(null, post)} >Edit</a>
+              <a href="#" onClick={deletePost.bind(null, post.id)} className={"delete-btn"}>Delete</a>
             </div>
           </div>
           </div>
